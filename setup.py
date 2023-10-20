f42cebdd8185ee86a52fb4e850ca68bec84497ab
slkpddm <slkpddm@gmail.com>
2023-10-20 06:34:52 +0530

    logging and utils functions were defined

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
+REPO_NAME = "Text-Summarizer"
 AUTHOR_USER_NAME = "Kulayappa"
-SRC_REPO = "TextSummarizer"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "slkpddm@gmail.com"
 
 
